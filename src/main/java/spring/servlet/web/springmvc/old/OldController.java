53bfaaa7032b97c0e82306596ef80af014d33298
JuseungL <juseung0619@gmail.com>
2023-07-26 15:34:11 +0900

    Spring MVC 구조 이해

---
+++ src/main/java/spring/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,16 @@
+package spring.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+@Component("/springmvc/old-controller") //스프링 빈(OldController)의 이름을 urlPattern으로 맞춰서 등록
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");
+    }
+}
